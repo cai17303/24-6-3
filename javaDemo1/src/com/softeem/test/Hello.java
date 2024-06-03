b5e5ded5e0c6c49a08b40206be0b92b5edff22f2
cai <1730377685@qq.com>
2024-06-03 20:28:34 +0800

    我要提交的文件

--- javaDemo1/src/com/softeem/test/Hello.java
+++ javaDemo1/src/com/softeem/test/Hello.java
@@ -3,5 +3,6 @@ package com.softeem.test;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
